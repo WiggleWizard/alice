7876f4c4ee5c34d1cc69a20114fc8d2596b3bac7
Zinglish <zinglish@gmail.com>
2015-05-14 09:54:25 +0100

    Added customization to player list plugin

--- plugins/player_list/plugin.py
+++ plugins/player_list/plugin.py
@@ -1,6 +1,7 @@
 from system.base_plugin import BasePlugin
 
 from system.player import Player
+from string import Template
 
 class Plugin(BasePlugin):
 	
@@ -9,6 +10,12 @@ class Plugin(BasePlugin):
 	requires     = ['advanced_commands']
 
 	def on_plugin_init(self):
+		# Config registers
+		self._fmt_entry = self.config.get('entry_format', "[${id}] ${clean_name}^7")
+		self._entries_per_page = self.config.get('entries_per_page', 6)
+		self._logged_in_color = self.config.get('logged_in_color', "^2")
+		self._logged_out_color = self.config.get('logged_out_color', "^4")
+
 		# Register command
 		command_plugin = self.get_plugin("advanced_commands")
 		command_plugin.register_command(self.command_p, 'p', 0);
@@ -33,13 +40,22 @@ class Plugin(BasePlugin):
 			page = int(args[0])
 			for player in self._alice._players: # print a page of 5 players
 				if player != None:
-					if i >= page*5 and i < (page+1)*5:
-						executor.tell(str(player.get_id())+"|"+player.get_clean_name()+"|"+player.get_ip())
+					if i >= page * self._entries_per_page and i < (page+1) * self._entries_per_page:
+						executor.tell(self._format_entry(player))
 					i+=1
 		else:
 			for player in self._alice._players: # print all, no parameter
 				if player != None:
-					executor.tell(str(player.get_id())+"|"+player.get_clean_name()+"|"+player.get_ip())
+					executor.tell(self._format_entry(player))
 
-	
-		
+	##
+	# Decorates with the player's information.
+	##
+	def _format_entry(self, player):
+		d = dict(
+			id         = player.get_id(),
+			clean_name = player.get_clean_name(),
+			ip         = player.get_ip()
+		)
+
+		return Template(self._fmt_entry).safe_substitute(d)
