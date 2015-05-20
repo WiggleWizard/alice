5c2481e4d310f890b3f79a126e370bcd53b61dd7
Terence-Lee Davis <terence-lee.davis@edelman.com>
2015-05-20 20:18:23 +0100

    Working on new advanced commands plugin

--- plugins/adv_commands/plugin.py
+++ plugins/adv_commands/plugin.py
@@ -1,65 +1,66 @@
-import os
-import imp
-import argparse
-import system.globals
-
 from system.base_plugin import BasePlugin
 
-# Internal
-CMD_STATUS_NO_CMD      = 0
-CMD_STATUS_SUCCESS     = 1
-CMD_STATUS_MISSING_ARG = 2
-CMD_STATUS_NO_PERMS    = 3
-CMD_STATUS_ARGS_TOO_LONG=4
+class Command(object):
 
-class ArgumentParserError(Exception): pass
+	CMD_STATUS_NO_CMD         = 0
+	CMD_STATUS_SUCCESS        = 1
+	CMD_STATUS_MISSING_ARG    = 2
+	CMD_STATUS_NO_PERMS       = 3
+	CMD_STATUS_ARGS_TOO_LONG  = 4
+	CMD_STATUS_ARGS_TOO_SHORT = 5
+	CMD_STATUS_ARG_PARSE_ERROR = 6
 
-class ThrowingArgumentParser(argparse.ArgumentParser):
-    def error(self, message):
-        raise ArgumentParserError(message)
+	def __init__(self, aliases, func, perm):
+		self._aliases = aliases
+		self._func    = func
+		self._perm    = perm
 
-class Command:
-	def __init__(self, name, command_method, perm, hint):
-		self._alias = name
-		self._parser = ThrowingArgumentParser(add_help=False)
-		self._command = command_method
-		self._perm = perm
-		self._hint = hint
-
-	def add_argument(self, arg_name, arg_type, hint, rep=1):
-		self._parser.add_argument(arg_name, type=arg_type, help=hint, nargs=rep)
-
-	def execute(self, executor, args):
-		#try:
-			pargs = self._parser.parse_args(args)
-			print args
-
-			self._command(executor, pargs);
-		#except ArgumentParserError as ape:
-		#	executor.tell("^1Invalid command semantics: " + str(ape))
-
-	def get_plugin(self, plugin_name):
-		plugin_manager = self._alice.get_plugin_manager()
-		return plugin_manager.get_plugin_instance(plugin_name)
+		self._parameters       = []
+		self._mandatory_params = 0
+		self._optional_params  = 0
+		self._has_free_text        = False
 		
+	def add_param(self, name, parse_method, hint, description, optional=False):
+		self._parameters.append({
+			'name': name,
+			'parse_method': parse_method,
+			'hint': hint,
+			'description': description
+		})
+
+		if optional:
+			self._optional_params++
+		else
+			self._mandatory_params++
+
+	def has_free_text(self, b=True):
+		self._has_free_text = b
+
+	def param_count(self):
+		return len(self._parameters)
+
+	def mandatory_param_count(self):
+		return self._mandatory_params
+
+	def optional_param_count(self):
+		return self._optional_params
 
 
 class Plugin(BasePlugin):
 
 	name         = 'advanced_commands'
 	display_name = 'Advanced Commands'
-	version      = '1.0'
-
-	def __init__(self):
-		self._commands = []
+	version      = '2.0a'
 
 	def on_plugin_init(self):
+		self._commands = []
 		self._director = self.config.get("directive", "!")
-#		self._commands_dir = system.globals.PLUGINS_PATH + '/adv_commands_data/'
 
-	def register_command(self, cmd):
+	def register_command(self, aliases, func, perm):
+		cmd = Command(aliases, func, perm)
 		self._commands.append(cmd)
 
+		return cmd
 
 	##
 	# Triggered when a player chats.
@@ -123,32 +124,49 @@ class Plugin(BasePlugin):
 	# 	$return            [int]    - See CMD_STATUS_... for returns
 	##
 	def _attempt_exec(self, player, cmd_issued, arg_string):
-
-		cmd_candidate = [];
 		for command in self._commands:
-			if command._alias == cmd_issued:
-				if command._perm != None:
-					if not player.has_perm(command._perm):
-						return CMD_STATUS_NO_PERMS
+			# Commands can have multiple aliases, so we check for them here
+			if cmd_issued in command._aliases:
+				# If the player doesn't have the perms then move on
+				if command._perm != None and not player.has_perm(command._perm):
+					return CMD_STATUS_NO_PERMS
 
-				# Attempt to split the args according to how the command
-				# specified.
-				#split_max = command['argc']
+				args_list = []
+				free_text = None
 
-				if(len(arg_string) > 100):
-					return CMD_STATUS_ARGS_TOO_LONG
+				# Break the input arg string into free text and actual args
+				arg_str_split = arg_string.split(":")
+				s = len(arg_str_split)
 
-				args = arg_string.split(" ")
+				if s > 0:
+					args_list = arg_str_split[0].trim().split(" ")
+				if s = 2:
+					free_text = arg_str_split[1].trim()
 
-				if args[0] == "":
-					args = []
+				# If the amount of arguments parsed does not match the required
+				# amount of args the command requires then send an error.
+				if len(args_list) < command.mandatory_param_count():
+					return Command.CMD_STATUS_ARGS_TOO_SHORT
+				if len(args_list) > command.param_count():
+					return Command.CMD_STATUS_ARGS_TOO_LONG
+
+				# Loop through the parameters of the command and add each
+				# one to the dictionary and call the parsing function
+				# on each one too.
+				args = {}
+				i = 0
+				for arg in args_list:
+					r = command._parameters[i].parse_method(arg)
+
+					if not r:
+						return Command.CMD_STATUS_ARG_PARSE_ERROR
+
+					args[command._parameters[i].name] = r
+					
+					i++
 
 				try:
 					command.execute(player, args);
-				except ArgumentParserError as ape:
-					cmd_candidate.append(command._hint+": " + str(ape))
-					continue
-					#executor.tell("^1Invalid command semantics: " + str(ape))
 				
 				# If the command returns no status then we automatically
 				# assume it handled its own output.
@@ -157,6 +175,7 @@ class Plugin(BasePlugin):
 					cmd_result = CMD_STATUS_SUCCESS
 
 				return cmd_result
-
-		player.tell("^1Invalid command semantics, candidates are: " + " ".join(cmd_candidate))
 		return CMD_STATUS_NO_CMD
+
+	def param_parse_player_search(self, search):
+		return []
